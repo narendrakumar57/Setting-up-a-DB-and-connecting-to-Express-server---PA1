8ffedca53f520e73149e31a62d0e7522a5a05f4d
narendrakumar57 <narendrakumarsuresh57@gmail.com>
2025-02-08 09:41:43 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require("dotenv").config()
 const express = require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = process.env.port || 3010;
 
 app.use(express.static('static'));
+app.use(cors())
+mongoose
+     .connect(process.env.db_URL,{ useNewUrlParser:true,useUnifiedTopology:true})
+     .then(()=> console.log("Connected to database"))
+     .catch((err)=> console.error("Error connecing to daatabase:",err))
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
